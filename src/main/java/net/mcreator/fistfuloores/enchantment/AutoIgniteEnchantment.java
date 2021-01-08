91b369ff98e9acc8b847ccd3d609f39c9a6191ca
ToasterBox834 <tomat@192.168.20.37>
2021-01-08 17:10:31 +1100

    Added AutoIgnite tnt enchantment

---
+++ src/main/java/net/mcreator/fistfuloores/enchantment/AutoIgniteEnchantment.java
@@ -0,0 +1,54 @@
+
+package net.mcreator.fistfuloores.enchantment;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.inventory.EquipmentSlotType;
+import net.minecraft.enchantment.EnchantmentType;
+import net.minecraft.enchantment.Enchantment;
+
+import net.mcreator.fistfuloores.FistfulOOresModElements;
+
+@FistfulOOresModElements.ModElement.Tag
+public class AutoIgniteEnchantment extends FistfulOOresModElements.ModElement {
+	@ObjectHolder("fistful_o_ores:auto_ignite")
+	public static final Enchantment enchantment = null;
+	public AutoIgniteEnchantment(FistfulOOresModElements instance) {
+		super(instance, 101);
+	}
+
+	@Override
+	public void initElements() {
+		elements.enchantments.add(() -> new CustomEnchantment(EquipmentSlotType.MAINHAND).setRegistryName("auto_ignite"));
+	}
+	public static class CustomEnchantment extends Enchantment {
+		public CustomEnchantment(EquipmentSlotType... slots) {
+			super(Enchantment.Rarity.COMMON, EnchantmentType.ALL, slots);
+		}
+
+		@Override
+		public int getMinLevel() {
+			return 1;
+		}
+
+		@Override
+		public int getMaxLevel() {
+			return 1;
+		}
+
+		@Override
+		public boolean isTreasureEnchantment() {
+			return true;
+		}
+
+		@Override
+		public boolean isCurse() {
+			return true;
+		}
+
+		@Override
+		public boolean isAllowedOnBooks() {
+			return true;
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/fistfuloores/procedures/AutoIgniteProcedureProcedure.java
@@ -0,0 +1,106 @@
+package net.mcreator.fistfuloores.procedures;
+
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.world.BlockEvent;
+import net.minecraftforge.common.MinecraftForge;
+
+import net.minecraft.world.World;
+import net.minecraft.world.IWorld;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.item.ItemStack;
+import net.minecraft.entity.item.TNTEntity;
+import net.minecraft.entity.SpawnReason;
+import net.minecraft.entity.MobEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.ILivingEntityData;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.Entity;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.block.Blocks;
+
+import net.mcreator.fistfuloores.enchantment.AutoIgniteEnchantment;
+import net.mcreator.fistfuloores.FistfulOOresModElements;
+
+import java.util.Map;
+import java.util.HashMap;
+
+@FistfulOOresModElements.ModElement.Tag
+public class AutoIgniteProcedureProcedure extends FistfulOOresModElements.ModElement {
+	public AutoIgniteProcedureProcedure(FistfulOOresModElements instance) {
+		super(instance, 102);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+
+	public static void executeProcedure(Map<String, Object> dependencies) {
+		if (dependencies.get("entity") == null) {
+			if (!dependencies.containsKey("entity"))
+				System.err.println("Failed to load dependency entity for procedure AutoIgniteProcedure!");
+			return;
+		}
+		if (dependencies.get("x") == null) {
+			if (!dependencies.containsKey("x"))
+				System.err.println("Failed to load dependency x for procedure AutoIgniteProcedure!");
+			return;
+		}
+		if (dependencies.get("y") == null) {
+			if (!dependencies.containsKey("y"))
+				System.err.println("Failed to load dependency y for procedure AutoIgniteProcedure!");
+			return;
+		}
+		if (dependencies.get("z") == null) {
+			if (!dependencies.containsKey("z"))
+				System.err.println("Failed to load dependency z for procedure AutoIgniteProcedure!");
+			return;
+		}
+		if (dependencies.get("world") == null) {
+			if (!dependencies.containsKey("world"))
+				System.err.println("Failed to load dependency world for procedure AutoIgniteProcedure!");
+			return;
+		}
+		Entity entity = (Entity) dependencies.get("entity");
+		double x = dependencies.get("x") instanceof Integer ? (int) dependencies.get("x") : (double) dependencies.get("x");
+		double y = dependencies.get("y") instanceof Integer ? (int) dependencies.get("y") : (double) dependencies.get("y");
+		double z = dependencies.get("z") instanceof Integer ? (int) dependencies.get("z") : (double) dependencies.get("z");
+		IWorld world = (IWorld) dependencies.get("world");
+		ItemStack iteminHand = ItemStack.EMPTY;
+		iteminHand = ((entity instanceof LivingEntity) ? ((LivingEntity) entity).getHeldItemMainhand() : ItemStack.EMPTY);
+		if ((((iteminHand).getItem() == new ItemStack(Blocks.TNT, (int) (1)).getItem())
+				&& ((EnchantmentHelper.getEnchantmentLevel(AutoIgniteEnchantment.enchantment, (iteminHand))) > 0))) {
+			if (dependencies.get("event") != null) {
+				Object _obj = dependencies.get("event");
+				if (_obj instanceof Event) {
+					Event _evt = (Event) _obj;
+					if (_evt.isCancelable())
+						_evt.setCanceled(true);
+				}
+			}
+			if (world instanceof World && !world.getWorld().isRemote) {
+				Entity entityToSpawn = new TNTEntity(EntityType.TNT, world.getWorld());
+				entityToSpawn.setLocationAndAngles((x + 0.5), y, (z + 0.5), world.getRandom().nextFloat() * 360F, 0);
+				if (entityToSpawn instanceof MobEntity)
+					((MobEntity) entityToSpawn).onInitialSpawn(world, world.getDifficultyForLocation(new BlockPos(entityToSpawn)),
+							SpawnReason.MOB_SUMMONED, (ILivingEntityData) null, (CompoundNBT) null);
+				world.addEntity(entityToSpawn);
+			}
+			((iteminHand)).shrink((int) 1);
+		}
+	}
+
+	@SubscribeEvent
+	public void onBlockPlace(BlockEvent.EntityPlaceEvent event) {
+		Entity entity = event.getEntity();
+		Map<String, Object> dependencies = new HashMap<>();
+		dependencies.put("x", (int) event.getPos().getX());
+		dependencies.put("y", (int) event.getPos().getY());
+		dependencies.put("z", (int) event.getPos().getZ());
+		dependencies.put("px", entity.getPosX());
+		dependencies.put("py", entity.getPosY());
+		dependencies.put("pz", entity.getPosZ());
+		dependencies.put("world", event.getWorld().getWorld());
+		dependencies.put("entity", entity);
+		dependencies.put("event", event);
+		this.executeProcedure(dependencies);
+	}
+}
